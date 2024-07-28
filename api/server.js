4f9400131fc4049b53dc1e07943620585e6ed47e
Muhammad <92302413+MuhamadBoboev@users.noreply.github.com>
2024-07-28 21:53:03 +0500

    2

--- api/server.js
+++ api/server.js
@@ -1,31 +1,26 @@
-// See https://github.com/typicode/json-server#module
-import { create, router as _router, defaults, rewriter } from 'json-server'
+/* eslint-disable no-undef */
+// JSON Server module
+// eslint-disable-next-line no-undef, @typescript-eslint/no-var-requires
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("/db.json");
 
-const server = create()
+// Make sure to use the default middleware
+const middlewares = jsonServer.defaults();
 
-// Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
-
-// Comment out to allow write operations
-const router = _router('db.json')
-
-const middlewares = defaults()
-
-server.use(middlewares)
+server.use(middlewares);
 // Add this before server.use(router)
-server.use(rewriter({
-    '/api/*': '/$1'
-}))
-server.use(router)
+server.use(
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/*": "/$1",
+ })
+);
+server.use(router);
+// Listen to port
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+ console.log("JSON Server is running");
+});
 
 // Export the Server API
-// eslint-disable-next-line no-undef
-export default server
+module.exports = server;
