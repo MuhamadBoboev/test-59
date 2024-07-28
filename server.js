60a15dbc8cfc709cfcde33172ff799096a61b980
Muhammad <92302413+MuhamadBoboev@users.noreply.github.com>
2024-07-28 22:22:10 +0500

    6

--- server.js
+++ server.js
@@ -3,24 +3,12 @@
 // eslint-disable-next-line no-undef, @typescript-eslint/no-var-requires
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-
-// Make sure to use the default middleware
+const router = jsonServer.router('db.json'); // Убедитесь, что файл db.json существует
 const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-// Add this before server.use(router)
-server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/*": "/$1",
- })
-);
-server.use(router);
-// Listen to port
-server.listen(3000, () => {
- console.log("JSON Server is running");
-});
 
-// Export the Server API
-module.exports = server;
+server.use(middlewares);
+server.use(router);
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
